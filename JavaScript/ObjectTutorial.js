a8c7ac6f9fdc8e19d5236e589ff27bc7b1dad1a1
Nicholas Boyson <nicholasb@gentrack.com>
2019-12-06 15:40:09 +1100

    Object Tutorial Added

---
+++ JavaScript/ObjectTutorial.js
@@ -0,0 +1,5 @@
+// Create an object:
+var car = {type: "Fiat", model: "500", color: "white"};
+
+// Display some data from the object:
+document.getElementById("demo").innerHTML = "The car type is " + car.type;
