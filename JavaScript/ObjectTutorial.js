27ca665138d348520589d71cf1d159eb24ebfe4e
Nicholas Boyson <nicholas@nickboyson.com>
2020-01-05 14:53:09 +1100

    Added functions to my object

--- JavaScript/ObjectTutorial.js
+++ JavaScript/ObjectTutorial.js
@@ -1,5 +1,12 @@
 // Create an object:
-var car = {type: "Fiat", model: "500", color: "white"};
+var car = {
+    type: "Fiat", 
+    model: "500",        
+    color: "white",
+    typeModel : function() {
+        return this.type + " " + this.model;
+    }
+};
 
 // Display some data from the object:
-document.getElementById("demo").innerHTML = "The car type is " + car.type;
+document.getElementById("demo").innerHTML = "The car is a " + car.typeModel();
